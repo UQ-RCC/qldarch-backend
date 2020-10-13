848127166cd62363d185867fa18d6a66488000fe
Alvin SW <a.sebastian@connect.qut.edu.au>
2020-10-13 15:36:41 +1000

    Consolidate WsArchObjs API

--- src/net/qldarch/archobj/WsArchObjs.java
+++ src/net/qldarch/archobj/WsArchObjs.java
@@ -46,22 +46,22 @@ public class WsArchObjs {
   @Nullable
   private User user;
 
-  @GET
-  @Path("/entities")
-  @Produces(ContentType.JSON)
-  @JsonSkipField(type=Media.class, field="depicts")
-  public Response getEntities() throws Exception {
-    if (user == null || !(user.isAdmin() || user.isEditor())) {
-      return Response.status(403).entity(M.of("msg", "Unauthorised user")).build();
-    }
-    String sql = "SELECT a.id as id, label, type, pubts, architect " +
-                 "FROM archobj a " +
-                 "LEFT JOIN appuser u ON a.owner = u.id " +
-                 "LEFT JOIN person p ON a.id = p.id " +
-                 "WHERE type NOT IN ('article', 'interview') AND deleted IS NULL AND trim(label) <> '' ORDER BY label ASC;";
-    List<Map<String, Object>> results = db.executeQuery(sql, Rsc::fetchAll);
-    return Response.ok().entity(results).build();
-  }
+  // @GET
+  // @Path("/entities")
+  // @Produces(ContentType.JSON)
+  // @JsonSkipField(type=Media.class, field="depicts")
+  // public Response getEntities() throws Exception {
+  //   if (user == null || !(user.isAdmin() || user.isEditor())) {
+  //     return Response.status(403).entity(M.of("msg", "Unauthorised user")).build();
+  //   }
+  //   String sql = "SELECT a.id as id, label, type, pubts, architect " +
+  //                "FROM archobj a " +
+  //                "LEFT JOIN appuser u ON a.owner = u.id " +
+  //                "LEFT JOIN person p ON a.id = p.id " +
+  //                "WHERE type NOT IN ('article', 'interview') AND deleted IS NULL AND trim(label) <> '' ORDER BY label ASC;";
+  //   List<Map<String, Object>> results = db.executeQuery(sql, Rsc::fetchAll);
+  //   return Response.ok().entity(results).build();
+  // }
 
   //@Inject
   //private TranscriptRelationshipSetup transcriptSetup;
@@ -70,28 +70,42 @@ public class WsArchObjs {
   @Produces(ContentType.JSON)
   @JsonSkipField(type=Media.class, field="depicts")
   public Response get(
-    @QueryParam("type") String type,
+    @QueryParam("type") List<String> types,
+    @QueryParam("exctype") List<String> exctypes,
+    @QueryParam("architect") Boolean architect,
     @QueryParam("owner") Long owner,
     @QueryParam("deleted") Boolean deleted,
     @QueryParam("published") Boolean published,
+    @DefaultValue("label") @QueryParam("sort") String sort,
     @DefaultValue("0") @QueryParam("offset") int offset,
-    @DefaultValue("100") @QueryParam("size") int size
+    @DefaultValue("1000") @QueryParam("size") int size
   ) throws Exception {
     if (user == null || !(user.isAdmin() || user.isEditor())) {
       return Response.status(403).entity(M.of("msg", "Unauthorised user")).build();
+    }
+    sort = sort.toLowerCase();
+    switch (sort) {
+      case "id": case "label": case "type": 
+        break;
+      default:
+        sort = "label";
+      break; 
     }
     String sql = "SELECT a.id as id, label, type, owner, u.displayname as ownername, deleted, pubts, architect " +
                  "FROM archobj a " +
                  "LEFT JOIN appuser u ON a.owner = u.id " +
                  "LEFT JOIN person p ON a.id = p.id " +
-                 "%s ORDER BY id ASC LIMIT :size OFFSET :offset;";
+                 "%s ORDER BY " + sort + " ASC LIMIT :size OFFSET :offset;";
     ArrayList<String> filters = new ArrayList<String>();
     HashMap<String, Object> params = new HashMap<String, Object>();
     params.put("offset", offset);
     params.put("size", size);
-    if (type != null) {
-      filters.add("type = :type");
-      params.put("type", type);
+    if (types != null && types.size() > 0) {
+      filters.add("type = ANY (:types)");
+      params.put("types", types);
+    } else if (exctypes != null && exctypes.size() > 0) {
+      filters.add("NOT (type = ANY (:exctypes))");
+      params.put("exctypes", exctypes);
     }
     if (owner != null) {
       filters.add("owner = :owner");
@@ -103,10 +117,14 @@ public class WsArchObjs {
     if (published != null) {
       filters.add("pubts is " + (published ? "not" : "") + " null");
     }
+    if (architect != null) {
+      filters.add("architect = " + (architect ? "true" : "false"));
+    }
     String cond = String.join(" AND ", filters);
     if (cond.length() > 0) cond = "WHERE " + cond;
 
     List<Map<String, Object>> results = db.executeQuery(String.format(sql, cond), params, Rsc::fetchAll);
+    return Response.ok().entity(results).build();
 /*
     final Stream<ArchObj> results = hs.getStream(session -> {
       CriteriaBuilder cb = session.getCriteriaBuilder();
@@ -120,29 +138,28 @@ public class WsArchObjs {
       return q.stream();
     });
     */
-    return Response.ok().entity(results).build();
   }
 
-  @GET
-  @Path("/owned")
-  @Produces(ContentType.JSON)
-  public List<Map<String, Object>> get(@QueryParam("type") String type) {
-    String sql = "SELECT a.id as id, label, type, deleted, pubts, architect FROM archobj a " +
-                 "LEFT JOIN person p ON a.id = p.id " +
-                 "WHERE owner = :id %s ORDER BY id ASC";
-    try {
-      if(user != null) {
-        if (type != null) {
-          return db.executeQuery(String.format(sql, "AND type = :type"), M.of("id", user.getId(), "type", type), Rsc::fetchAll);
-        } else {
-          return db.executeQuery(String.format(sql, ""), M.of("id", user.getId()), Rsc::fetchAll);
-        }
-      } else {
-        //log.debug("media by owner failed as user is {}", user);
-      }
-    } catch(Exception e) {
-      //log.debug("media by owner failed for user {} ", user, e);
-    }
-    return null;
-  }
+  // @GET
+  // @Path("/owned")
+  // @Produces(ContentType.JSON)
+  // public List<Map<String, Object>> get(@QueryParam("type") String type) {
+  //   String sql = "SELECT a.id as id, label, type, deleted, pubts, architect FROM archobj a " +
+  //                "LEFT JOIN person p ON a.id = p.id " +
+  //                "WHERE owner = :id %s ORDER BY id ASC";
+  //   try {
+  //     if(user != null) {
+  //       if (type != null) {
+  //         return db.executeQuery(String.format(sql, "AND type = :type"), M.of("id", user.getId(), "type", type), Rsc::fetchAll);
+  //       } else {
+  //         return db.executeQuery(String.format(sql, ""), M.of("id", user.getId()), Rsc::fetchAll);
+  //       }
+  //     } else {
+  //       //log.debug("media by owner failed as user is {}", user);
+  //     }
+  //   } catch(Exception e) {
+  //     //log.debug("media by owner failed for user {} ", user, e);
+  //   }
+  //   return null;
+  // }
 }
